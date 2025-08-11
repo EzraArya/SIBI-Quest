1b557450135c8d8dcfc70c74f7a42ecbf31ed8dc
EzraArya <ezrawijaya10@gmail.com>
2025-08-11 20:57:27 +0700

    feat: Add AppColors

---
+++ lib/shared/tokens/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF673AB7);
+  static const Color secondary = Color(0xFFC3B1E1);
+  static const Color accent = Color(0xFF9B5DE5);
+
+  static const Color background = Color(0xFF0F0B1B);
+  static const Color text = Color(0xFFF1DFF4);
+  static const Color placeholder = Color(0xFFBCAFD0);
+  static const Color error = Color(0xFFEF476F);
+  static const Color textbox = Color(0xFF18122B);
+  static const Color complementary = Color(0xFFFFB547);
+  static const Color muted = Color(0xFF2A1F3A);
+  static const Color line = Color(0xFF5C33A5);
+  static const Color trackbar = Color(0xFF2A1D3C);
+}
