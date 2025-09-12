87ddc09c82f89a5b37ba165dd55d6784b2dc2662
Samuel Benediktus Meliala <samuelmeliala04@gmail.com>
2025-09-12 11:22:28 +0700

    feat: Add login page

--- lib/features/auth/auth_router.dart
+++ lib/features/auth/auth_router.dart
@@ -1,15 +1,23 @@
 import 'package:go_router/go_router.dart';
 import 'package:sibi_quest/features/auth/presentation/pages/login_page.dart';
+import 'package:sibi_quest/features/auth/presentation/pages/signup_page.dart';
 
 class AuthRoutes {
   static const String loginName = 'login';
   static const String loginPath = '/login';
+  static const String signupName = 'signup';
+  static const String signupPath = '/signup';
 
   static List<GoRoute> routes() => [
     GoRoute(
       name: loginName,
       path: loginPath,
       builder: (context, state) => const LoginPage(),
+    ),
+    GoRoute(
+      name: signupName,
+      path: signupPath,
+      builder: (context, state) => const SignupPage(),
     ),
   ];
 }
